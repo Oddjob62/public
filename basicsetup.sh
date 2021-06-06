985dfee74208684b749d2ddf26375b74c19937b7
Alex Britton <alex@ahbritton.com>
2021-06-06 12:19:18 +0100

    Create basicsetup.sh

---
+++ basicsetup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt-get install nginx -y
+
+# mkdir -p ~/download
+# cd ~/download
+# wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+# rpm -ivh epel-release-latest-7.noarch.rpm
+# yum -y install cowsay
+# cowsay ExaGridDba
